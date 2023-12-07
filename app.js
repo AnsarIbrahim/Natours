1620fecaba5c00539e927e36e4900eac5edb3cd6
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-07 14:22:34 +0530

    Handling PATCH Requests

--- app.js
+++ app.js
@@ -61,6 +61,28 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  // console.log(req.params);
+  // console.log(req.body);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);

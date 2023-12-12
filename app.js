1d82d2439be755616483ec5c477f1aeadc0cb71f
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-12 19:20:44 +0530

    Create and Mounting Routes

--- app.js
+++ app.js
@@ -122,23 +122,21 @@ const deleteUser = (req, res) => {
 
 // 3) ROUTES
 
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
 app.param('id', checkId);
 
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
+tourRouter.route('/').get(getAllTours).post(createTour);
 
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
+userRouter.route('/').get(getAllUsers).post(createUser);
 
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 // 4) START SERVER
 

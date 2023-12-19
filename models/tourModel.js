efb6c35186e11379d139e3cc5417fff56baceba4
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-19 23:48:56 +0530

    Data Validation with BuiltIn

--- models/tourModel.js
+++ models/tourModel.js
@@ -7,6 +7,10 @@ const tourSchema = new mongoose.Schema(
       type: String,
       required: [true, 'A tour must have a name'],
       unique: true,
+      trim: true, // Remove all white spaces in the beginning and end of the string
+      maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+      minlength: [10, 'A tour name must have more or equal than 10 characters'],
+      // validate: [validator.isAlpha, 'Tour name must only contain characters'],
     },
     slug: String,
     duration: {
@@ -20,10 +24,17 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        // Only for strings
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium or difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'A tour must have a rating above or equal to 1.0'],
+      max: [5, 'A tour must have a rating below or equal to 5.0'],
     },
     ratingsQuantity: {
       type: Number,

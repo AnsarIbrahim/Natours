cbe15ab86e5779fb5f1e93c5431f87464a4c8871
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-19 23:30:50 +0530

    Query Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -54,6 +54,10 @@ const tourSchema = new mongoose.Schema(
       select: false, // Hide this field from the output
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -80,6 +84,19 @@ tourSchema.pre('save', (next) => {
 //   console.log(doc);
 //   next();
 // });
+
+// QUERY MIDDLEWARE: runs before .find() and .findOne()
+tourSchema.pre(/^find/, (next) => {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, (docs, next) => {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  console.log(docs);
+  next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 

b43b4bc0993a090d3bf580057a1041f79c176055
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-20 10:43:23 +0530

    Implement Global Error Handleing

--- app.js
+++ app.js
@@ -4,21 +4,6 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-// const checkId = (req, res, next, val) => {
-//   exports.id = val * 1;
-//   exports.tour = tours.find((el) => el.id === id);
-
-//   if (!tour) {
-//     return res.status(404).json({
-//       status: 'fail',
-//       message: 'Invalid ID',
-//     });
-//   }
-
-//   req.tour = tour;
-//   next();
-// };
-
 const app = express();
 
 // MIDDLEWARES
@@ -27,12 +12,37 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
-
 app.use(express.static(`${__dirname}/public`));
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 // ROUTES
-// app.param('id', checkId);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500; // 500 = Internal Server Error
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;

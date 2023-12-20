a10132ff6f9b2beb4c2f925eb088f80f9f161fe8
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-20 14:49:56 +0530

    Handle Mongoose Valdition Errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -44,6 +44,14 @@ const handleDuplicateFieldsDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = (err) => {
+  // const errors = Object.values(err.errors).map((el) => el.message);
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+
+  return new AppError(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500; // 500 = Internal Server Error
   err.status = err.status || 'error';
@@ -54,6 +62,7 @@ module.exports = (err, req, res, next) => {
     // let error = { ...err };
     if (err.name === 'CastError') err = handleCastErrorDB(err);
     if (err.code === 11000) err = handleDuplicateFieldsDB(err);
+    if (err.name === 'ValidationError') err = handleValidationErrorDB(err);
 
     sendErrorProd(err, res);
   }

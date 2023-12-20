e27b8ca03712edfd8665681912cab5e86aa2c568
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-20 14:41:07 +0530

    Handle Duplicate Database Fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -36,6 +36,14 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  // const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const value = err.keyValue.name;
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+
+  return new AppError(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500; // 500 = Internal Server Error
   err.status = err.status || 'error';
@@ -45,6 +53,7 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === 'production') {
     // let error = { ...err };
     if (err.name === 'CastError') err = handleCastErrorDB(err);
+    if (err.code === 11000) err = handleDuplicateFieldsDB(err);
 
     sendErrorProd(err, res);
   }

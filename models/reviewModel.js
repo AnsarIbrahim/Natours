28bc923b04163aa89ddc055f8d31cdfaedf70cbe
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-22 18:18:21 +0530

    Populate Reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -38,6 +38,23 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'tour',
+    select: 'name',
+  }).populate({
+    path: 'user',
+    select: 'name photo',
+  });
+
+  //   this.populate({
+  //     path: 'user',
+  //     select: 'name photo',
+  //   });
+
+  next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;

c77b58c14eb65b481538cbb921667bf78de2a707
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-22 17:01:30 +0530

    Tour Guides Embedding

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -104,6 +105,13 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: [
+      // Array of IDs
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
@@ -125,6 +133,13 @@ tourSchema.pre('save', function (next) {
   if (typeof this.name === 'string') {
     this.slug = slugify(this.name, { lower: true });
   }
+  next();
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+
   next();
 });
 

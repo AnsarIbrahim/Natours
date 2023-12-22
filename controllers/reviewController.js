26bd5be8a2e79d5a5a56bea502d068d19fca6a21
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-22 18:10:10 +0530

    Create and Get Reviews

---
+++ controllers/reviewController.js
@@ -0,0 +1,43 @@
+const Review = require('../models/reviewModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const reviews = await Review.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: reviews.length,
+    data: {
+      reviews,
+    },
+  });
+});
+
+exports.createReview = catchAsync(async (req, res, next) => {
+  const newReview = await Review.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      review: newReview,
+    },
+  });
+});
+
+exports.getReview = catchAsync(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new AppError(`No review found with that ID: ${req.params.id}`, 404),
+    );
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      review,
+    },
+  });
+});

511932f93bfc20eeded36a4e84be61ab06b86177
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-24 19:17:58 +0530

    Improving Read Performance with Indexes

--- models/tourModel.js
+++ models/tourModel.js
@@ -119,6 +119,11 @@ const tourSchema = new mongoose.Schema(
   },
 );
 
+// tourSchema.index({ price: 1 });
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+tourSchema.index({ startLocation: '2dsphere' }); // For geospatial queries
+
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });

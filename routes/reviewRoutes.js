5cf442edd8f728c652c5987fa2fc5a68372e4eab
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-24 18:41:20 +0530

    Adding Missing Auth and Authorization

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -6,11 +6,13 @@ const router = express.Router({
   mergeParams: true,
 });
 
+// Protect all routes after this middleware
+router.use(authController.protect);
+
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview,
@@ -19,7 +21,13 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
-  .delete(reviewController.deleteReview);
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview,
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview,
+  );
 
 module.exports = router;

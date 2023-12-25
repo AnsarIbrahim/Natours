294c3340a790c23af790a35289f865569537aa13
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-25 19:09:06 +0530

    Create Base Templeate with pug

--- app.js
+++ app.js
@@ -73,7 +73,10 @@ app.use((req, res, next) => {
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Ansar',
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);

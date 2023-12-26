049d9fb79797b23d3541eb71a3cc278b4a5c2b65
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-26 12:35:56 +0530

    Build the tour Page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -15,7 +15,17 @@ exports.getOverview = catchAsync(async (req, res) => {
 });
 
 exports.getTour = catchAsync(async (req, res) => {
+  // 1) Get the data, for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
+  // 2) Build template
+
+  // 3) Render template using tour data from 1)
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour,
   });
 });

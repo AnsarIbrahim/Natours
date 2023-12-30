ac3663d29f387a2d2c5117d44a1f81320e751530
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-30 13:47:54 +0530

    Resize Images

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,39 @@
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
 
 // Multer
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/img/users');
-  },
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/img/users');
+//   },
 
-  filename: (req, file, cb) => {
-    // user-7676767asdasd-123123123.jpeg
-    const ext = file.mimetype.split('/')[1];
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-  },
-});
+//   filename: (req, file, cb) => {
+//     // user-7676767asdasd-123123123.jpeg
+//     const ext = file.mimetype.split('/')[1];
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//   },
+// });
+
+const multerStorage = multer.memoryStorage();
 
 const upload = multer({ storage: multerStorage });
 
 // Middleware
 exports.uploadUserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = (req, res, next) => {
+  if(!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  sharp(req.file.buffer).resize(500, 500).toFormat('jpeg').jpeg({ quality: 90 }).toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+}
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};

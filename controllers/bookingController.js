165804168cb056b6392bb3031ccdc07d4a524404
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2024-01-02 18:38:06 +0530

    Bookings

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -3,7 +3,6 @@ const catchAsync = require('../utils/catchAsync');
 const Tour = require('../models/tourModel');
 const Booking = require('../models/bookingModel');
 const factory = require('./handlerFactory');
-const AppError = require('../utils/appError');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.tourId);
@@ -44,3 +43,9 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 
   res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -14,6 +14,12 @@ router.get(
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
+router.get(
+  '/my-tours',
+  // bookingController.createBookingCheckout,
+  authController.protect,
+  viewsController.getMyTours,
+);
 router.post(
   '/submit-user-data',
   authController.protect,

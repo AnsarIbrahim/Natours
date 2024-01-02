497fa5edec5061f316e0d0eae32c051b237e31f9
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2024-01-02 19:02:20 +0530

    Prepared app for deployment

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const { contentSecurityPolicy } = require('helmet');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -69,6 +70,8 @@ app.use(
     ],
   }),
 );
+
+app.use(compression());
 
 // Test middleware
 app.use((req, res, next) => {
